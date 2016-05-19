9e3e9b2bd6f11e213a56a0ced1588c6996103334
Mihail Costraci <mihaicostraci3@gmail.com>
2016-05-19 09:16:19 +0300

    Pie Chart in progress.

--- Statistic/Statistic/STChartViewController.swift
+++ Statistic/Statistic/STChartViewController.swift
@@ -32,13 +32,13 @@ class STChartViewController: STBaseViewController {
 	override func viewDidAppear(animated: Bool) {
 		super.viewDidAppear(animated)
 		
-		self.getStatistic()
+//		self.getStatistic()
 		
 		daysOfWeek = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"]
 		let unitsSold = [8.0, 5.6, 0.0, 0.0, 0.0, 0.0, 0.0]
 		
 		setChart(daysOfWeek, values: unitsSold)
-		setPieChart()
+//		setupPieChartViewS()
 		
 	}
 	
@@ -86,13 +86,52 @@ class STChartViewController: STBaseViewController {
 		barChartView.data = chartData
 		
 		chartDataSet.colors = [NSUIColor.orangeColor()]
+        barChartView.animate(xAxisDuration: 1.0, yAxisDuration: 1.0, easingOption: .EaseInOutBounce)
 		
 	}
-	
-	func setPieChart() {
-		
-		
-		pieChartView.drawRect(CGRectMake(30, 30, 40, 40))
-	}
+//	
+//    func setupPieChartViewS(chartView: PieChartView) {
+//    
+//    chartView.usePercentValuesEnabled = true;
+//    chartView.drawSlicesUnderHoleEnabled = false;
+//    chartView.holeRadiusPercent = 0.58;
+//    chartView.transparentCircleRadiusPercent = 0.61;
+//    chartView.descriptionText = "";
+////    [chartView.setExtraOffsetsWithLeft:5.f top:10.f right:5.f bottom:5.f];
+//    chartView.setExtraOffsets(left: 5.0, top: 10.0, right: 5.0, bottom: 5.0)
+//        
+//    chartView.drawCenterTextEnabled = true;
+//        var paragraphStyle: NSMutableParagraphStyle = NSParagraphStyle.defaultParagraphStyle().mutableCopy() as! NSMutableParagraphStyle
+//
+//    paragraphStyle.lineBreakMode = .ByTruncatingTail;
+//    paragraphStyle.alignment = .Center;
+//    
+//    var centerText: NSMutableAttributedString = NSMutableAttributedString(string: "iOS Charts\nby Daniel Cohen Gindi")
+//    [centerText setAttributes:@{
+//    NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue-Light" size:12.f],
+//    NSParagraphStyleAttributeName: paragraphStyle
+//    } range:NSMakeRange(0, centerText.length)];
+//    [centerText addAttributes:@{
+//    NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue-Light" size:10.f],
+//    NSForegroundColorAttributeName: UIColor.grayColor
+//    } range:NSMakeRange(10, centerText.length - 10)];
+//    [centerText addAttributes:@{
+//    NSFontAttributeName: [UIFont fontWithName:@"HelveticaNeue-LightItalic" size:10.f],
+//    NSForegroundColorAttributeName: [UIColor colorWithRed:51/255.f green:181/255.f blue:229/255.f alpha:1.f]
+//    } range:NSMakeRange(centerText.length - 19, 19)];
+//    chartView.centerAttributedText = centerText;
+//    
+//    chartView.drawHoleEnabled = YES;
+//    chartView.rotationAngle = 0.0;
+//    chartView.rotationEnabled = YES;
+//    chartView.highlightPerTapEnabled = YES;
+//    
+//    ChartLegend *l = chartView.legend;
+//    l.position = ChartLegendPositionRightOfChart;
+//    l.xEntrySpace = 7.0;
+//    l.yEntrySpace = 0.0;
+//    l.yOffset = 0.0;
+//    }
+
 	
 }
